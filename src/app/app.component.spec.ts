414418272db8e711121a757f5e2e6292fd42e7b0
SantiagoRTI <snlopezl@rtisas.com.co>
2024-09-24 21:47:11 -0500

    nuevo proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Proyect-QA' title`, () => {
+  it(`should have the 'autoQA-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Proyect-QA');
+    expect(app.title).toEqual('autoQA-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Proyect-QA');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, autoQA-frontend');
   });
 });
